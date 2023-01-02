d2f070b0ef360a1c187496e1db960d4a34169058
Nimisha Mahadevan <76494912+Nimisha-30@users.noreply.github.com>
2023-01-02 21:13:09 +0530

    Add files via upload

---
+++ CafeManagementSystem/userinfo.h
@@ -0,0 +1,12 @@
+#ifndef USERINFO_H_INCLUDED
+#define USERINFO_H_INCLUDED
+
+using namespace std;
+
+// Structure will be used to collect and manipulate information from user
+struct userInfo {
+    int userID;
+    int password;
+};
+
+#endif // USERINFO_H_INCLUDED
